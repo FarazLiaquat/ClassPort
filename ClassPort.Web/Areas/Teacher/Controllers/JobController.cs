633e68fa66030e5770cb775643c3f07106c20468
Chris <jetsboy2011@gmail.com>
2022-03-30 10:19:34 -0400

    fro fil

---
+++ ClassPort.Web/Areas/Teacher/Controllers/JobController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ClassPort.Web.Controllers;
+using RoverCore.BreadCrumbs.Services;
+
+namespace ClassPort.Web.Areas.Teacher.Controllers
+{
+    [Area("Teacher")]
+    [Authorize(Roles="Teacher")]
+    public class JobController : BaseController<JobController>
+    {
+        public IActionResult Index()
+        {
+	        _breadcrumbs.StartAtAction("Dashboard", "Index", "Home", new { Area = "Dashboard" })
+		        .Then("Teacher")
+		        .ThenAction("Hangfire Dashboard", "Index", "Job", new { Area = "Admin" });
+
+            return View();
+        }
+    }
+}
